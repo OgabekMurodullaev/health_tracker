c9f95a8aff2fd7e339efd12413591782f7c64a8c
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2025-04-08 16:05:30 +0500

    changed

--- core/settings.py
+++ core/settings.py
@@ -83,14 +83,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '5432',
-    }
+    'default': os.getenv("DATABASE_URL")
 }
 
 
